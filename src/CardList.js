a136f85a443d243320d98196dbb0030a57b6fe22
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-06-29 09:53:37 +0200

    can compare selected words

--- src/CardList.js
+++ src/CardList.js
@@ -6,41 +6,102 @@ class CardList extends Component {
   constructor() {
     super();
     this.state = {
+      selected_from_word_id: null,
+      selected_to_word_id: null,
       words: [
         { id: 1,
           from: 'en',
-          to: 'on'
+          to: 'on',
+          correct_guesses: 0,
         },
         { id: 2,
           from: 'to',
-          to: 'two'
+          to: 'two',
+          correct_guesses: 0,
         },
         { id: 3,
           from: 'tre',
-          to: 'three'
+          to: 'three',
+          correct_guesses: 0,
         },
         { id: 4,
           from: 'fire',
-          to: 'four'
+          to: 'four',
+          correct_guesses: 0,
         }
       ]
     }
   };
 
+
+  resetSelectedWords() {
+    console.log('reset');
+    this.setState({
+      selected_from_word_id: null,
+      selected_to_word_id: null
+    })
+  }
+
+  compareSelections() {
+    if (this.state.selected_from_word_id === this.state.selected_to_word_id) {
+      console.log('correct word')
+    } else {
+      console.log('NOT correct word')
+    }
+    this.resetSelectedWords()
+  };
+
+  wordSelected(word, side) {
+    if (side  === 'from') {
+      this.setState({
+        selected_from_word_id: word.id
+      });
+    }
+
+    if (side  === 'to') {
+      this.setState(
+        { selected_to_word_id: word.id },
+        this.compareSelections
+      );
+    }
+
+    console.log("State is: ", this.state);
+
+
+  }
+
   render() {
 
-      debugger;
-    var list = this.state.words.map((word, index) => (
-      <li data-index={index} key={index}> {word.from} </li>
+
+    var from_list = this.state.words.map((word, index) => (
+      <li
+        data-index={index}
+        key={index}
+        onClick={ this.wordSelected.bind(this, word, 'from') }>
+        {word.from}
+      </li>
+    ))
+
+    var to_list = this.state.words.map((word, index) => (
+      <li
+        data-index={index}
+        key={index}
+        onClick={ this.wordSelected.bind(this, word, 'to') }>
+        {word.to}
+      </li>
     ))
 
 
     return (
       <div className="CardList">
         <h1> Flash Cards </h1>
-        <ul>
-          {list}
-        </ul>
+
+        <h3> From </h3>
+        <ul> {from_list} </ul>
+
+        <h3> To </h3>
+        <ul> {to_list} </ul>
+
       </div>
     )
   }
