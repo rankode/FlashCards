4f8c6a10a34fedc59fd49f8ca721beb356e0c97b
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-06-30 09:28:07 +0200

    Added Array.prototype.shuffle

--- src/index.js
+++ src/index.js
@@ -4,5 +4,17 @@ import CardList from './CardList';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+Array.prototype.shuffle = function() {
+  var i = this.length, j, temp;
+  if ( i === 0 ) return this;
+  while ( --i ) {
+     j = Math.floor( Math.random() * ( i + 1 ) );
+     temp = this[i];
+     this[i] = this[j];
+     this[j] = temp;
+  }
+  return this;
+}
+
 ReactDOM.render(<CardList />, document.getElementById('root'));
 registerServiceWorker();
