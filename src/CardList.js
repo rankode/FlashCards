878ccd808bd7a5bedd9b411a0b15e63f897bdffc
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-06-30 09:27:39 +0200

    remove mocked list of words

--- src/CardList.js
+++ src/CardList.js
@@ -45,28 +45,7 @@ class CardList extends Component {
     this.state = {
       selected_from_word_id: null,
       selected_to_word_id: null,
-      words: [
-        { id: 1,
-          from: 'en',
-          to: 'on',
-          correct_guesses: 0,
-        },
-        { id: 2,
-          from: 'to',
-          to: 'two',
-          correct_guesses: 0,
-        },
-        { id: 3,
-          from: 'tre',
-          to: 'three',
-          correct_guesses: 0,
-        },
-        { id: 4,
-          from: 'fire',
-          to: 'four',
-          correct_guesses: 0,
-        }
-      ]
+      words: [],
     }
   };
 
